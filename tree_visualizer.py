b539eb2d3f733d76801656fadf470a89813eeccc
evan <epmoring@gmail.com>
2022-05-17 23:18:27 -0700

    Cleanup

--- tree_visualizer.py
+++ tree_visualizer.py
@@ -1,23 +1,6 @@
 from copy import deepcopy
-example_dict = {
-'pets':{
-    'cat': 
-        {'frita':{},
-        'george':{},
-        'carl':{}},
-    'dog':  
-        {'fido':{}}
-},
-'children':{
-    'fam1':
-        {'frita':{},
-        'george':{},
-        'carl':{}},
-    'fam2': {},
-    }
-}
 
-def viz_tree(dictionary, depth = [], has_parent = False, has_sibling = False):
+def viz_tree(dictionary, depth = [], has_parent = False):
     for index, key in enumerate(dictionary):
         string = ''
         new_depth = deepcopy(depth)
@@ -26,17 +9,12 @@ def viz_tree(dictionary, depth = [], has_parent = False, has_sibling = False):
 
         first = (index == 0)
         last = (index == len(dictionary) - 1)
+
         if first and last:
-            if has_sibling:
-                if not has_parent:
-                    string += "┌"
-                else:
-                    string += "├"
+            if not has_parent:
+                string += "─"
             else:
-                if not has_parent:
-                    string += "─"
-                else:
-                    string += "└"
+                string += "└"
             new_depth.append('  ')
 
         elif first:
@@ -49,6 +27,7 @@ def viz_tree(dictionary, depth = [], has_parent = False, has_sibling = False):
         elif last:
             string += "└"
             new_depth.append('  ')
+
         else:    
             string += '├'
             new_depth.append('│ ')
@@ -61,16 +40,9 @@ def viz_tree(dictionary, depth = [], has_parent = False, has_sibling = False):
             pass
         string += key
         print(string)
-        has_children(dictionary, key)
         child_dictionary = dictionary[key]
             
-        has_sibling = False#(list_index != len(dictionary))
-        viz_tree(child_dictionary, new_depth, has_parent = True, has_sibling =has_sibling)
-
-def viz_tree_2(dictionary):
-    for i in dictionary:
-        print(i)
-        viz_tree_2(dictionary[i])
+        viz_tree(child_dictionary, new_depth, has_parent = True)
 
 def has_children(dictionary, key):
     if len(dictionary[key]):
@@ -78,8 +50,24 @@ def has_children(dictionary, key):
     else:
         return False
 
-viz_tree(example_dict, has_sibling = False)
-#viz_tree_2(example_dict)
+example_dict = {
+'pets':{
+    'cat': 
+        {'frita':{},
+        'george':{},
+        'carl':{}},
+    'dog':  
+        {'fido':{}}
+},
+'children':{
+    'fam1':
+        {'frita':{}},
+    'fam2': {},
+    }
+}
+
+if __name__ == '__main__':
+    viz_tree(example_dict)
 
 # ┌├└│ ─
 # unicode: http://www.unicode.org/charts/PDF/U2500.pdf
